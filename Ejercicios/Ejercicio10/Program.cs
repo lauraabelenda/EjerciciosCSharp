1564bdf99e9c000cee1bef0c0408dece4d101bc0
i8592 <i8592@WKSFIN259>
2019-01-15 09:22:31 +0100

    Ejercicios C#

--- Ejercicios/Ejercicio10/Program.cs
+++ Ejercicios/Ejercicio10/Program.cs
@@ -17,7 +17,7 @@ namespace Ejercicio10
 
             for (int i = 1; i <= 100; i++)
             {
-                if (i%2 == 0 || i%3==0)
+                if (i%2 == 0 || i%3 == 0)
                 {
                     Console.WriteLine(i);
                 }
